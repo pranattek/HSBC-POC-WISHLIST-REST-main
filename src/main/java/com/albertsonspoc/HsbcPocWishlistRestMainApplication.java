a65bec1822bd5a55fa850f8e5bc81f257f255113
pmund <pranatmund92@gmail.com>
2021-11-02 20:03:16 +0530

    added logger

--- src/main/java/com/albertsonspoc/HsbcPocWishlistRestMainApplication.java
+++ src/main/java/com/albertsonspoc/HsbcPocWishlistRestMainApplication.java
@@ -1,10 +1,14 @@
 package com.albertsonspoc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HsbcPocWishlistRestMainApplication {
+
+	public static Logger log = LoggerFactory.getLogger(HsbcPocWishlistRestMainApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HsbcPocWishlistRestMainApplication.class, args);
